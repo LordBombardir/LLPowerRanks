744da7933ce77a76c2054cf1eeda26c172765378
LordBombardir <lordanon6@gmail.com>
2025-01-01 17:32:20 +0300

    debugging..

--- src/mod/hooks/Hooks.cpp
+++ src/mod/hooks/Hooks.cpp
@@ -2,7 +2,6 @@
 #include "../Utils.hpp"
 #include "../manager/MainManager.h"
 #include "../manager/command/CommandManager.h"
-#include <ll/api/io/LoggerRegistry.h>
 #include <ll/api/memory/Hook.h>
 #include <mc/network/PacketSender.h>
 #include <mc/network/ServerNetworkHandler.h>
@@ -25,9 +24,6 @@ LL_TYPE_INSTANCE_HOOK(
     ServerPlayer&            player
 ) {
     origin(source, connectionRequest, player);
-    
-    std::shared_ptr<ll::io::Logger> logger = ll::io::LoggerRegistry::getInstance().getOrCreate("TEST");
-    logger->info("Test 1...");
 
     manager::MainManager::updatePlayerRank(player);
 }
@@ -127,11 +123,11 @@ LL_TYPE_INSTANCE_HOOK(
 
 void Hooks::setupHooks() {
     ServerNetworkHandlerSendLoginMessageLocalHook::hook();
-    CommandRegistryAddEnumValueConstraintsHook::hook();
-    CommandRegistryCheckOriginCommandFlagsHook::hook();
-    CommandRunHook::hook();
+    //CommandRegistryAddEnumValueConstraintsHook::hook();
+    //CommandRegistryCheckOriginCommandFlagsHook::hook();
+    //CommandRunHook::hook();
 
-    PlayerSendMessageHook::hook();
+    //PlayerSendMessageHook::hook();
 }
 
 } // namespace power_ranks::hooks
