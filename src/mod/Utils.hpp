9001cda5d3d6c5809f81c61e1fb5c59bd163bfea
LordBombardir <lordanon6@gmail.com>
2025-02-01 17:58:53 +0300

    Fix strReplace

--- src/mod/Utils.hpp
+++ src/mod/Utils.hpp
@@ -19,29 +19,41 @@ public:
         return str;
     }
 
-    static std::string
-    strReplace(std::string originalStr, const std::string& whatNeedToReplace, const std::string& whatForReplace) {
-        originalStr.replace(originalStr.find(whatNeedToReplace), whatNeedToReplace.length(), whatForReplace);
-        return originalStr;
+    static std::string strReplace(
+        const std::string& originalStr,
+        const std::string& whatNeedToReplace,
+        const std::string& whatForReplace
+    ) {
+        std::string result = originalStr;
+
+        size_t pos = 0;
+        while ((pos = result.find(whatNeedToReplace, pos)) != std::string::npos) {
+            result.replace(pos, whatNeedToReplace.length(), whatForReplace);
+            pos += whatForReplace.length();
+        }
+
+        return result;
     }
 
     static std::string strReplace(
-        std::string                     originalStr,
+        const std::string&              originalStr,
         const std::vector<std::string>& whatNeedToReplace,
         const std::vector<std::string>& whatForReplace
     ) {
+        std::string result = originalStr;
         if (whatNeedToReplace.size() != whatForReplace.size()) {
             throw std::invalid_argument("Vectors «whatNeedToReplace» and «whatForReplace» must have the same size!");
         }
 
+        // Заменяем каждую подстроку
         for (size_t i = 0; i < whatNeedToReplace.size(); ++i) {
-            const std::string& target      = whatNeedToReplace[i];
-            const std::string& replacement = whatForReplace[i];
+            const std::string& searchFor   = whatNeedToReplace[i];
+            const std::string& replaceWith = whatForReplace[i];
 
-            originalStr = strReplace(originalStr, target, replacement);
+            result = strReplace(result, searchFor, replaceWith);
         }
 
-        return originalStr;
+        return result;
     }
 
     template <typename T>
