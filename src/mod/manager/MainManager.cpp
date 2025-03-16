4314e14ab5b396f7fd6ca6097a34d7e363741a6e
LordBombardir <lordanon6@gmail.com>
2025-03-16 13:40:27 +0300

    Update to LeviLamina@1.1.1 & fix setScoreTag

--- src/mod/manager/MainManager.cpp
+++ src/mod/manager/MainManager.cpp
@@ -6,9 +6,15 @@
 #include "ranks/RanksManager.h"
 #include <LLTranslatorApi.h>
 #include <ll/api/service/Bedrock.h>
+#include <mc/entity/components/SynchedActorDataComponent.h>
 #include <mc/server/commands/CommandRegistry.h>
+#include <mc/world/actor/ActorDataIDs.h>
+#include <mc/world/actor/DataItem.h>
+#include <mc/world/actor/SynchedActorData.h>
+#include <mc/world/actor/SynchedActorDataEntityWrapper.h>
 #include <mc/world/actor/player/LayeredAbilities.h>
 #include <mc/world/level/Level.h>
+
 
 // wth mojang?
 AvailableCommandsPacket::EnumData::EnumData(const EnumData&)                                     = default;
@@ -122,11 +128,13 @@ void MainManager::setPlayerRankByXuid(const std::string& xuid, const object::Ran
 }
 
 void MainManager::updatePlayerRank(Player& player) {
-    const object::Rank&               rank     = manager::MainManager::getPlayerRankOrSetDefault(player);
-    Bedrock::Safety::RedactableString scoreTag = Bedrock::Safety::RedactableString();
+    const object::Rank& rank = manager::MainManager::getPlayerRankOrSetDefault(player);
 
-    scoreTag = Utils::strReplace(rank.getScoreTagFormat(), "{prefix}", rank.getPrefix());
-    player.setRedactableNameTag(scoreTag);
+    std::string                scoreTag = Utils::strReplace(rank.getScoreTagFormat(), "{prefix}", rank.getPrefix());
+    std::unique_ptr<DataItem>& dataScoreTag =
+        player.mEntityData->mData->mData->mItemsArray->at(static_cast<size_t>(ActorDataIDs::Score));
+
+    dataScoreTag->setData<std::string>(std::move(scoreTag));
 
     extraActions(rank, player);
     extraVanillaActions(player, rank);
